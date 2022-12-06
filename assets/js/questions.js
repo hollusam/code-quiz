8dba1aff251d1a54aab78499beeb8092ce17634f
Hollusam <oluyomisamuels@gmail.com>
2022-12-06 17:14:19 +0000

    array of questions added to the question.js file

---
+++ assets/js/questions.js
@@ -0,0 +1,43 @@
+//Create array of questions and correct
+var quizQuestion = [
+    {
+      questionTitle: "Commonly used data types DO NOT include:",
+      choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+      correct: "3. alerts",
+    },
+    {
+      questionTitle: "Arrays in JavaScript can be used to store ______.",
+      choices: [
+        "1. numbers and strings",
+        "2. other arrays",
+        "3. booleans",
+        "4. all of the above",
+      ],
+      correct: "4. all of the above",
+    },
+    {
+      questionTitle:
+        "String values must be enclosed within _____ when being assigned to variables.",
+      choices: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
+      correct: "3. quotes",
+    },
+    {
+      questionTitle:
+        "A very useful tool used during development and debugging for printing content to the debugger is:",
+      choices: [
+        "1. JavaScript",
+        "2. terminal/bash",
+        "3. for loops",
+        "4. console.log",
+      ],
+      correct: "4. console.log",
+    },
+    {
+      questionTitle:
+        "Which of the following is a statement that can be used to terminate a loop, switch or label statement?",
+      choices: ["1. break", "2. stop", "3. halt", "4. exit"],
+      correct: "1. break",
+    },
+  ];
+
+  export {quizQuestion};
